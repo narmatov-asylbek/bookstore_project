425ec534b4f3c20fcaf3ef350f803c4bb540dec4
asyl <pochemuno@gmail.com>
2021-03-18 12:08:27 +0600

    finished adding crispy forms and static files like javascript, css, images

--- pages/tests.py
+++ pages/tests.py
@@ -2,7 +2,7 @@ from django.http import response
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 
-from .views import HomePageView
+from .views import HomePageView, AboutPageView
 
 class HomepageTest(SimpleTestCase):
     
@@ -25,3 +25,22 @@ class HomepageTest(SimpleTestCase):
     def test_homepage_resolves_homepageview(self):
         view = resolve('/')
         self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
+
+
+class AboutPageTest(SimpleTestCase):
+    def setUp(self):
+        url = reverse('about')
+        self.response = self.client.get(url)
+
+    def test_about_page_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_about_page_template(self):
+        self.assertTemplateUsed(self.response, 'about.html')
+
+    def test_about_page_text_content(self):
+        self.assertContains(self.response, 'Bookstore project')
+    
+    def test_about_page_resolve_aboutpage(self):
+        view = resolve('/about/')
+        self.assertEqual(view.func.__name__, AboutPageView.as_view().__name__)
