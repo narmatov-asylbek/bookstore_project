ca8b70ef44d88ac44c460356ef1c9a1f073f11c1
asyl <pochemuno@gmail.com>
2021-03-18 15:09:53 +0600

    created environment and .gitignore file

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_i-2v-8g9_yb!+leav%i1akwr^wy$z3rx_oxu3k=if&=sw0uag'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
 ALLOWED_HOSTS = []
 
