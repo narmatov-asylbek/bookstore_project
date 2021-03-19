d85ccfdb3fa16edce41f2b16c821317d78b229f9
asyl <pochemuno@gmail.com>
2021-03-19 17:47:34 +0600

    created a search form

--- books/urls.py
+++ books/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import BookPageView, BookDetailView
+from .views import BookPageView, BookDetailView, SearchListView
 
 urlpatterns = [
     path('', BookPageView.as_view(), name='book_list'),
-    path('<uuid:pk>/', BookDetailView.as_view(), name="book_details")
+    path('<uuid:pk>/', BookDetailView.as_view(), name="book_details"),
+    path('search/', SearchListView.as_view(), name='search_results')
 ]
